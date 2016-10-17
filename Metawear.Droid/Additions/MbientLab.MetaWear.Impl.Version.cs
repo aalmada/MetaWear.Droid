ac0f778de342cc718a26fedb329102903000193b
Antao Almada <antao.almada@gmail.com>
2016-10-17 22:09:34 +0100

    Interfaces with overriden methods still not compiling

---
+++ Metawear.Droid/Additions/MbientLab.MetaWear.Impl.Version.cs
@@ -0,0 +1,10 @@
+namespace MbientLab.MetaWear.Impl
+{
+    public partial class Version : global::Java.Lang.IComparable
+    {
+        int global::Java.Lang.IComparable.CompareTo(global::Java.Lang.Object another)
+        {
+            return CompareTo((global::MbientLab.MetaWear.Impl.Version)another);
+        }
+    }
+}
